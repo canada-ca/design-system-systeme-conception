a0abd217d29ba0125a1bb33a15fd7dbb63ff8e36
Borges-Porteous <miguel.borges-porteous@tbs-sct.gc.ca>
2019-08-12 15:10:06 -0400

    updated checkboxes in contact pattern

--- src/contact-checkboxes-label.js
+++ src/contact-checkboxes-label.js
@@ -1,12 +1,16 @@
 // If all checkboxes with the same name are checked, and there is a label with a related class name, check
 $(document).ready(function(){
-  $("#checklist-completion-check").change(function(field){
+  $(".checklist-completion-check").change(function(field){
     var sName = field.target.name;
     var checks = $('input[name="'+ sName +'"]');
     if (checks.length === checks.filter(":checked").length) {
-      document.getElementById(sName+'-message').innerHTML = '<label class="label label-success">You are ready to call</label>';
+      $("#"+sName+"-message").html('You are ready');
+      $("#"+sName+"-label").removeClass('label-danger');
+      $("#"+sName+"-label").addClass('label-success');
     } else {
-      document.getElementById(sName+'-message').innerHTML = '<label class="label label-danger">You need all of the above to call</label>';
+      $("#"+sName+"-message").html('You need all of the above');
+      $("#"+sName+"-label").removeClass('label-success');
+      $("#"+sName+"-label").addClass('label-danger');
     }
   });
 });
